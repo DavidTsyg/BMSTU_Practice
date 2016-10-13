29ec6330d3211b2ea7a78c3fbfc168fc84b870fa
DavidTsyg <davidtsyg@gmail.com>
2016-10-13 23:23:24 +0300

    Update zadacha1.cpp

--- zadacha1.cpp
+++ zadacha1.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
     float y;
     float x;
-    cout <<"Введите x ";
+    cout <<"Введите x : ";
     cin >> x;
     y = (pow(x,5) + pow(x,3) + 2)/(pow(x,2) + 10);
-    cout <<"Вывод " << y;
+    cout <<"Вывод : " << y;
     get.h;
 }
