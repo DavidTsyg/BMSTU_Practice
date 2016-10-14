6884ce2cf54dea636946827e7202ac3d35a20f18
DavidTsyg <davidtsyg@gmail.com>
2016-10-14 15:39:43 +0400

    Update zadacha2.cpp

--- zadacha2.cpp
+++ zadacha2.cpp
@@ -6,8 +6,8 @@ int main()
 {
     int x1, x2, x3, x4, x5, y1, y2, y3, y4, y5;
     float L21, L31, L41, L51, min;
-    std::cout << "Введите координаты 5 точек\n";
-    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4 >> x5 >> y5;
+    cout << "Введите координаты 5 точек\n";
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4 >> x5 >> y5;
     L21 = sqrt(pow(x2-x1,2) + pow(y2-y1,2));
     L31 = sqrt(pow(x3-x1,2) + pow(y3-y1,2));
     L41 = sqrt(pow(x4-x1,2) + pow(y4-y1,2));
@@ -20,6 +20,6 @@ int main()
     if (L31 == min) std::cout << "На минимальном расстоянии находится точка " << x3 << " " << y3 << endl;
     if (L41 == min) std::cout << "На минимальном расстоянии находится точка " << x4 << " " << y4 << endl;
     if (L51 == min) std::cout << "На минимальном расстоянии находится точка " << x5 << " " << y5 << endl;
-    get.h;
+    _getсh();
     return 0;
 }
